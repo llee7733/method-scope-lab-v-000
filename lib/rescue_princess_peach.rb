70016fcb3f952b497480e85137c8c4cf59fd8efe
Lloyd Lee <kinglloydlee@gmail.com>
2018-08-10 01:32:21 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,2 +1,8 @@
-#rescue_princess_peach
-#puts status
+def rescue_princess_peach
+  status = "rescued"
+  puts "Hooray! Mario has rescued Princess Peach."
+end
+
+
+rescue_princess_peach
+puts status
